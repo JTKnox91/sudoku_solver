9afd31c65cc8eb2cd2616d434929c30ac865fd8c
JTKnox91 <jtknox91@gamil.com>
2015-07-06 14:48:27 -0700

    Renamed sudoku objects with "s" prefix to avoid confusion with native DOM .rows/.cells

--- sudoku.js
+++ sudoku.js
@@ -1,46 +1,46 @@
-	//Grid object
-	function Grid() {
-		this.cells = [];
-		this.rows = [];
-		this.columns = [];
+	//Sudoku Grid object
+	function SGrid() {
+		this.sCells = [];
+		this.sRows = [];
+		this.sColumns = [];
 		this.boxes = [];
 		for (var i = 0; i < 81; i++) {
-			this.cells.push(new Cell(i));
+			this.sCells.push(new SCell(i));
 		}
 		for (var i = 0; i < 9; i++) {
-			this.rows.push(new Row(i));
-			this.columns.push(new Column(i));
-			this.boxes.push(new Box(i));
+			this.sRows.push(new SRow(i));
+			this.sColumns.push(new SColumn(i));
+			this.boxes.push(new SBox(i));
 		}
 	}
 
-	//Cell object
-	function Cell(num) {
-		this.row = Math.floor(num/9);
-		this.column = (num+9)%9;
-		this.box = 3 * Math.floor(this.row/3) + Math.floor(this.column/3);
+	//Sudoku Cell object
+	function SCell(num) {
+		this.sRow = Math.floor(num/9);
+		this.sColumn = (num+9)%9;
+		this.box = 3 * Math.floor(this.sRow/3) + Math.floor(this.sColumn/3);
 	}
-	//Row object
-	function Row(num) {
-		this.cells = [];
+	//Sudoku Row object
+	function SRow(num) {
+		this.sCells = [];
 		for (var i = 0; i < 9; i++) {
-			cells.push(num+i);
+			sCells.push(num+i);
 		}
 	}
-	//Column object
-	function Column(num) {
-		this.cells = [];
+	//Sudoku sColumn object
+	function SColumn(num) {
+		this.sCells = [];
 		for(var i = 0; i < 9; i++) {
-			cells.push(num + 9*i);
+			sCells.push(num + 9*i);
 		}
 	}
-	//Box object
+	//Sudoku Box object
 	function Box(num) {
-		this.cells = [];
-		var firstCell = 9 * Math.floor(num/3) + 3 * num%3;
+		this.sCells = [];
+		var firstsCell = 9 * Math.floor(num/3) + 3 * num%3;
 		for (var i = 0; i < 3; i++) {
 			for (var j = 0; j < 3; j++) {
-				this.cells.push(firstCell + 9*i + j);
+				this.sCells.push(firstsCell + 9*i + j);
 			} 
 		}
 	}
